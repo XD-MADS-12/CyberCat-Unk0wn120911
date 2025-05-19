169ca0220ec8b7d73d625cdfa129ad6fecaa4456
XD ATIK VAU <145763843+XD-MADS-12@users.noreply.github.com>
2025-05-19 10:31:06 +0600

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© Cyber Cat Unkn0wn | For Educational Use Only</p>
+      <div className="social-links">
+        <a href="https://github.com/XD-MADS-12">GitHub</a> | <a href="#">Twitter</a> | <a href="@Anon_Lucifer44">Telegram</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
