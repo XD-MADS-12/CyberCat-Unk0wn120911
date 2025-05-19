0754b703737289e57c75dddd7a074210a3ac1b9e
XD ATIK VAU <145763843+XD-MADS-12@users.noreply.github.com>
2025-05-19 10:28:15 +0600

    Create Header.jsx

---
+++ Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1 className="glow">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Cat_paw_print.svg" className="logo-img" />
+        Cyber Cat Unkn0wn
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
